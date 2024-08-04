6fea691598a854a7e47a45626aecb763ce70b8c7
Dovalkin <tranquangtuananh2801@gmail.com>
2024-08-04 21:58:16 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// const apiKey = "your_api_key_here";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
